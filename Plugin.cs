98d032b800dfa702eaf1adf78cbc48c8ae04418f
Nick <11450606+nd0905@users.noreply.github.com>
2024-05-03 16:03:04 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -21,7 +21,7 @@ namespace cs2_rockthevote
     public partial class Plugin : BasePlugin, IPluginConfig<Config>
     {
         public override string ModuleName => "RockTheVote";
-        public override string ModuleVersion => "1.8.5";
+        public override string ModuleVersion => "1.8.6";
         public override string ModuleAuthor => "abnerfs";
         public override string ModuleDescription => "https://github.com/abnerfs/cs2-rockthevote";
 
